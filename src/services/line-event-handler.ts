edfc7ff4bb3419889a5e8b477e272bdc067ab22a
yoshikouki <yoshikouki@gmail.com>
2022-12-04 00:26:19 +0900

    Reply to incoming message with OpenAI GPT-3

--- src/services/line-event-handler.ts
+++ src/services/line-event-handler.ts
@@ -1,10 +1,20 @@
-import { WebhookEvent } from "@line/bot-sdk";
+import { EventMessage, WebhookEvent } from "@line/bot-sdk";
 import { lineClient } from "../lib/line";
+import openAi from "../lib/openai";
 
 export const lineEventHandler = async (event: WebhookEvent) => {
   if (event.type === "message") {
     const { message, replyToken } = event
-    const text = message.type === "text" ? message.text : "テキストメッセージを送信してください";
+    const text = await generateReplyMessage(message)
     return lineClient.replyMessage(replyToken, { type: "text", text });
+  }
+}
+
+const generateReplyMessage = async (message: EventMessage) => {
+  if (message.type === "text") {
+    const replyMessage = await openAi.conversation(message.text);
+    return replyMessage ? replyMessage.trim() : "(返事ありませんでした...)"
+  } else {
+    return "テキストメッセージを送信してください"
   }
 }

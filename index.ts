cb6c4d3b9b30a9bf6e88f1156c4339ca829b4eac
yoshikouki <yoshikouki@gmail.com>
2022-12-03 16:22:43 +0900

    Create to talk interactively with OpenAI

--- index.ts
+++ index.ts
@@ -1,9 +1,35 @@
 import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+import { createInterface } from "readline";
 
 config();
 
-const openAiApiKey: string = process.env.OPENAI_API_KEY || ""
-const openai: string = "Hello, openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-console.log(openAiApiKey);
-console.log(openai);
+;
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "> ",
+});
+
+const conversation = async (input: string) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: input,
+    temperature: 1.0,
+    max_tokens: 128,
+  });
+  return response.data.choices[0].text;
+};
+
+readline.prompt();
+readline.on("line", async (input) => {
+  const reply = await conversation(input);
+  console.log(reply);
+  console.log("\n--------------------\n");
+  readline.prompt();
+});

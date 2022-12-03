7d3975756f0e5bdbaa5f38eec082a4209b97485f
yoshikouki <yoshikouki@gmail.com>
2022-12-03 22:37:41 +0900

    Format

--- src/config.ts
+++ src/config.ts
@@ -5,9 +5,9 @@ dotenv.config();
 const config = {
   openAi: {
     apiKey: process.env.OPENAI_API_KEY || "",
-    model: process.env.OPENAI_MODEL  || "text-davinci-003",
-    temperature: Number(process.env.OPENAI_TEMPERATURE)  || 1.0,
-    max_tokens: Number(process.env.OPENAI_MAX_TOKENS)  || 128,
+    model: process.env.OPENAI_MODEL || "text-davinci-003",
+    temperature: Number(process.env.OPENAI_TEMPERATURE) || 1.0,
+    max_tokens: Number(process.env.OPENAI_MAX_TOKENS) || 128,
   },
   server: {
     port: Number(process.env.PORT) || 3000,

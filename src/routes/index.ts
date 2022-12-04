9d8c071b5a1255e1b40aaaff6fbeac908ea91c5c
yoshikouki <yoshikouki@gmail.com>
2022-12-04 11:56:08 +0900

    Add health check endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,8 @@ import { WebhookRequestBody } from "@line/bot-sdk";
 import { lineEventHandler } from "../services/line-event-handler";
 
 export const setupRoutes = (app: Express) => {
+  app.get("/health", (_req, res) => res.send("OK"))
+
   app.post(
     "/v1/webhooks/line",
     async (req: { body: WebhookRequestBody }, res) => {
